82b150223c667661c26431faffb0ac6a0f529682
crawld <danielcrawl@gmail.com>
2016-04-15 10:35:41 -0700

    run yum as root

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-yum install -y python-matplotlib libreoffice-calc python-imaging python-pip
+sudo yum install -y python-matplotlib libreoffice-calc python-imaging python-pip
 
 pip install tweepy --user
 
