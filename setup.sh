621eb96894c5c1a26c1f321c73b641d227db6d1d
Alok Singh <a1singh@ucsd.edu>
2016-04-19 14:16:00 -0700

    setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,7 @@ sudo yum install -y python-matplotlib libreoffice-calc python-imaging python-pip
 pip install tweepy --user
 
 pip install textblob --user
+
+wget http://archive.apache.org/dist/lucene/java/5.5.0/lucene-5.5.0.tgz
+
+tar -xvzf lucene-5.5.0.tgz
