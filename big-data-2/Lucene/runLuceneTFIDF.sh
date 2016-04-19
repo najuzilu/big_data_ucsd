46a1c16630fa613a9c12ddfa7d1728aa07dc9d68
Alok Singh <a1singh@ucsd.edu>
2016-04-19 14:09:09 -0700

    scripts to run java

---
+++ big-data-2/Lucene/runLuceneTFIDF.sh
@@ -0,0 +1,8 @@
+#usage: . ./runLuceneIndexer.sh /path/to/Lucene
+#hint : do not put / in the end
+
+CLASSPATH=${CLASSPATH}:$1/core/*:$1/queryparser/*:$1/analysis/common/*:$1/demo/*
+
+export CLASSPATH
+ 
+java LuceneTFIDF
