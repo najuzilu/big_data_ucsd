79c53d9b42a93a38e0c1ddcbe865b7b27bfd2ca2
crawld <danielcrawl@gmail.com>
2016-04-19 12:40:33 -0700

    use position numbers

--- big-data-2/sensor/stream-data.py
+++ big-data-2/sensor/stream-data.py
@@ -6,5 +6,5 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('rtd.hpwren.ucsd.edu', 12020))
 for i in range(0, 60):
     d = s.recv(1024)
-    print "{}: {}".format(i, d)
+    print "{0}: {1}".format(i, d)
 s.close()
