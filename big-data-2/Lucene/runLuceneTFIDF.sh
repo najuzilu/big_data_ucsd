640b32b078879177e898dce2ff6bb16563c58b50
Alok Singh <a1singh@ucsd.edu>
2016-04-19 14:12:26 -0700

    adding scripts to run java and class files

--- big-data-2/Lucene/runLuceneTFIDF.sh
+++ big-data-2/Lucene/runLuceneTFIDF.sh
@@ -1,4 +1,4 @@
-#usage: . ./runLuceneIndexer.sh /path/to/Lucene
+#usage: . ./runLuceneTFIDF.sh /path/to/Lucene
 #hint : do not put / in the end
 
 CLASSPATH=${CLASSPATH}:$1/core/*:$1/queryparser/*:$1/analysis/common/*:$1/demo/*
