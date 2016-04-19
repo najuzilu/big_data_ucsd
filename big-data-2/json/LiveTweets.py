d8b7637613464fcfd29dc254a5cb2dc62924ee6c
Alok Singh <a1singh@ucsd.edu>
2016-04-19 11:29:57 -0700

    football

--- big-data-2/json/LiveTweets.py
+++ big-data-2/json/LiveTweets.py
@@ -4,11 +4,12 @@ import json
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler, Stream
 from login import *
+import sys
 
 class TweetStreamListener(StreamListener):
     def on_data(self, data):
         tweet = json.loads(data)
-        print "\n||" + tweet["created_at"][4:-10] + "|| " + tweet["text"][:65]
+        print "\n||" + tweet["created_at"][4:-10] + "|| " + tweet["text"][:90]
         return True
     
     def on_error(self, status):
@@ -18,4 +19,4 @@ listener = TweetStreamListener()
 auth_key = OAuthHandler(consumer_key, consumer_secret)
 auth_key.set_access_token(access_token, access_token_secret)
 live_twitter_stream = Stream(auth_key, listener)
-live_twitter_stream.filter(track=[arg])
+live_twitter_stream.filter(track=['football'])
