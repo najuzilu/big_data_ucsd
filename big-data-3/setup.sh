0beec5b43b2e839636b1f1bd2661024ecea0e2a4
crawl <danielcrawl@gmail.com>
2016-07-19 12:07:35 -0700

    adding postgres commands

--- big-data-3/setup.sh
+++ big-data-3/setup.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 
+sudo yum install -y postgresql-server
+
+sudo service postgresql initb
+sudo service postgresql start
+sudo -u postgres psql -c "CREATE USER cloudera"
+sudo -u postgres -c "ALTER USER cloudera with superuser"
+sudo -u postgres createdb cloudera
+cd postgres
+psql -f init-postgres.sql
+cd ..
