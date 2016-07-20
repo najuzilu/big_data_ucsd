a40b7fcb0415b86772760ab55d47c0eb11ce7322
crawld <cloudera@quickstart.cloudera>
2016-07-19 17:34:48 -0700

    adding commands for pyspark jupyter

--- big-data-3/setup.sh
+++ big-data-3/setup.sh
@@ -13,4 +13,5 @@ psql -f init-postgres.sql
 wget http://repo.continuum.io/archive/Anaconda3-4.0.0-Linux-x86_64.sh
 bash Anaconda3-4.0.0-Linux-x86_64.sh
 
-
+echo "export PYSPARK_DRIVER_PYTHON_OPTS=\"notebook\"" >> ~/.bashrc
+echo "export PYSPARK_DRIVER_PYTHON=jupyter"  >> ~/.bashrc
