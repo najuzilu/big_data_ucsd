fc9fb6db5d03985edf04c62c52335b3c4fe00b77
cloudera <cloudera@quickstart.cloudera>
2016-10-06 09:42:53 -0700

    fixing for MulticlassMetrics in hands on 4

--- big-data-4/setup.sh
+++ big-data-4/setup.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
 # upgrade spark python to work with python 3
-sudo yum upgrade -y spark-python
+sudo yum upgrade -y spark-python hive
 
-# update spark log4j to be quiet and restart
-sudo cp setup/log4j.properties /usr/lib/spark/conf/
-sudo chmod 644 /usr/lib/spark/conf/log4j.properties
-sudo service spark-master restart
-sudo service spark-worker restart
-
-# decompress csv files
-gzip -d *.csv.gz
+# remove old derby jar so only one version is on spark's classpath
+sudo rm /usr/lib/flume-ng/lib/derby-10.8.2.2.jar
 
 # download and install anaconda for pandas, jupyter
+rm -f Anaconda3-4.0.0-Linux-x86_64.sh
 wget http://repo.continuum.io/archive/Anaconda3-4.0.0-Linux-x86_64.sh
 bash Anaconda3-4.0.0-Linux-x86_64.sh
 
@@ -23,4 +18,4 @@ echo export SPARK_CLASSPATH="$(pwd)/lib/spark-csv_2.10-1.5.0.jar:$(pwd)/lib/comm
 echo "export PYSPARK_DRIVER_PYTHON_OPTS=\"notebook\"" >> ~/.bashrc
 echo "export PYSPARK_DRIVER_PYTHON=jupyter"  >> ~/.bashrc
 
-echo "Run 'source .bashrc' to complete the setup."
+echo "Run 'source ~/.bashrc' to complete the setup."
