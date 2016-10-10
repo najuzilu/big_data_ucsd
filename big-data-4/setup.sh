2b7c1e5421fb4942581e8b7c6584d22bef36ebcb
crawl <danielcrawl@gmail.com>
2016-10-10 10:45:43 -0700

    adding minute weather

--- big-data-4/setup.sh
+++ big-data-4/setup.sh
@@ -6,6 +6,9 @@ sudo yum upgrade -y spark-python hive
 # remove old derby jar so only one version is on spark's classpath
 sudo rm /usr/lib/flume-ng/lib/derby-10.8.2.2.jar
 
+# decompress datasets
+gzip -d *.csv.gz
+
 # download and install anaconda for pandas, jupyter
 rm -f Anaconda3-4.0.0-Linux-x86_64.sh
 wget http://repo.continuum.io/archive/Anaconda3-4.0.0-Linux-x86_64.sh
